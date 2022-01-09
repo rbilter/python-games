9e1084de888692610e4e0b4cbae350435ad5fa1f
Rich Bilter <rbilter@rxsense.com>
2022-01-09 13:51:34 -0500

    Clean up the game surface class

--- surface.py
+++ surface.py
@@ -36,16 +36,17 @@ class GameSurface():
         return self.__SCREEN_WIDTH
 
     def new_game_surface(self):
-        self.surface = pygame.display.set_mode((400, 600))
+        self.__surface = pygame.display.set_mode(
+            (self.__SCREEN_WIDTH, self.__SCREEN_HEIGHT))
         pygame.display.set_caption("Crazy Cars")
 
     def render_backgroud(self):
-        self.surface.blit(self.__background, (0, 0))
+        self.__surface.blit(self.__background, (0, 0))
 
     def render_collision(self, player):
         x = player.rect.centerx - (self.__collision.get_width() / 2)
         y = player.rect.top - (self.__collision.get_height() / 2)
-        self.surface.blit(self.__collision, (x, y))
+        self.__surface.blit(self.__collision, (x, y))
         pygame.display.update()
 
     def render_game_over(self, all_sprites):
@@ -53,8 +54,8 @@ class GameSurface():
         x = self.__get_x_center(s)
         y = self.__get_y_center(s)
 
-        self.surface.fill(self.__RED)
-        self.surface.blit(s, (x, y))
+        self.__surface.fill(self.__RED)
+        self.__surface.blit(s, (x, y))
         pygame.display.update()
 
         for entity in all_sprites:
@@ -62,36 +63,36 @@ class GameSurface():
 
     def render_game_score(self, score):
         s = self.__font_small.render(str(score), True, self.__BLACK)
-        self.surface.blit(s, (10, 560))
+        self.__surface.blit(s, (10, 560))
 
     def render_get_ready(self):
         s = self.__font.render("Get Ready!", True, self.__BLACK)
         x = self.__get_x_center(s)
         y = self.__get_y_center(s)
 
-        self.surface.fill(self.__BLUE)
-        self.surface.blit(s, (x, y))
+        self.__surface.fill(self.__BLUE)
+        self.__surface.blit(s, (x, y))
         pygame.display.update()
 
     def render_high_score(self, score):
         s = self.__font_small.render(str(score), True, self.__BLACK)
-        self.surface.blit(s, (10, 10))
+        self.__surface.blit(s, (10, 10))
 
     def render_new_high_score(self, score):
         s = self.__font_small.render(
             str(score) + ' IS A NEW HIGH SCORE!', True, self.__BLACK)
-        self.surface.blit(s, (self.__get_x_center(s), 10))
+        self.__surface.blit(s, (self.__get_x_center(s), 10))
 
     def render_play_again(self):
         s = self.__font_small.render(
             "Play Again? Yes(y) or No(n)", True, self.__BLACK)
         x = self.__get_x_center(s)
-        self.surface.blit(s, (x, 560))
+        self.__surface.blit(s, (x, 560))
         pygame.display.update()
 
     def render_sprites(self, all_sprites):
         for entity in all_sprites:
-            self.surface.blit(entity.image, entity.rect)
+            self.__surface.blit(entity.image, entity.rect)
             entity.update(self.__SCREEN_WIDTH, self.__SCREEN_HEIGHT)
 
     def update(self):
